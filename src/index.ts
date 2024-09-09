99a882203c796b5882117a5792d5ab07efa22c89
eubrunocoelho <eu.brunocoelho94@gmail.com>
2024-09-09 17:47:21 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log('Olá, mundo!'); // success, no error vscode
+console.log('Olá, mundo!') // forced vscode error
+console.log("Olá, mundo!"); // forced vscode error
+console.log("Olá, mundo!") // forced vscode error
